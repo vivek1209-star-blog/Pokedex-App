86d5e64bc11eb38018f15a5574ecf305c572eea5
vivek.mishra@codeb.online <vivek.mishra@codeb.online>
2022-07-05 18:32:42 +0530

    added icon and backround color

---
+++ src/Config/Index.js
@@ -0,0 +1,9 @@
+
+export const Image_Api_Url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+
+
+export const toFirstCharUppercase = name =>
+    name.charAt(0).toUpperCase() + name.slice(1)
+
+
+export const IMAGE_URL = "https://pokeres.bastionbot.org/images/pokemon"    
